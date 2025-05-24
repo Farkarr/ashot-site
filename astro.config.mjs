2a32633d71a34d4a8bde13cf24e887bee5cbb95b
farkar98 <adyankaro@gmail.com>
2025-05-24 18:05:38 +0200

    prueba para que funcionen las rutas con github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://Farkarr.github.io",
-  base: "ashot-site"
+  base: "/ashot-site"
 });
